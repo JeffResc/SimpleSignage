6fc38b4e47661877a5ba01cbf3fe8ca99b29d3ab
Jeff Rescignano <jeff@jeffresc.dev>
2020-06-18 00:37:21 -0400

    Initial commit

---
+++ passwordHasher.js
@@ -0,0 +1,6 @@
+const crypto = require('crypto');
+
+const pw = process.argv.slice(2)[0];
+const hash = crypto.createHash("sha256").update(pw).digest("base64");
+
+console.log('Hash for ' + pw + ' is: ' + hash);
