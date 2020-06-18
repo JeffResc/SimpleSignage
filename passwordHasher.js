5ff0528e24b9791a302819ec852bb7f964e4d3eb
Jeff Rescignano <jeff@jeffresc.dev>
2020-06-17 22:30:24 -0400

    a

--- passwordHasher.js
+++ passwordHasher.js
@@ -3,4 +3,4 @@ const crypto = require('crypto');
 const pw = process.argv.slice(2)[0];
 const hash = crypto.createHash("sha256").update(pw).digest("base64");
 
-console.log('Hash  for ' + pw + ' is: ' + hash);
+console.log('Hash for ' + pw + ' is: ' + hash);
